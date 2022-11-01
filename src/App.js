111e9d0c0143e6c74123543c3886d140b563e32f
Nitendra Rana <nitendrarana108@gmail.com>
2022-11-01 19:23:00 +0530

    updated api

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 import Page1 from "./Components/Pages/Page1";
 import axios from "axios";
-import {Route, Routes} from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 import Player from "./Components/Pages/Player";
 import ErrorPage from "./Components/Pages/ErrorPage";
 
-const URL = "https://api-uat.greatmanagerinstitute.com/api/v2/getTrailerList";
+// const URL = "https://api-uat.greatmanagerinstitute.com/api/v2/getTrailerList";
+const URL = "https://nitendra-rana.github.io/json-api/movies.json";
+
 function App() {
   const [movie, setMovie] = useState([]);
   const [player, setPlayer] = useState("");
@@ -16,19 +18,34 @@ function App() {
 
   useEffect(() => {
     async function fetchData() {
-      const response = await axios.get(URL);
+      const response = await axios.get(URL, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        
+      });
       setMovie(response.data.data);
     }
     fetchData();
+    
   }, []);
 
   return (
     <div>
       <Header />
       <Routes>
-      <Route path = "/" exact element={<Page1 movie={movie} setPlayer = {setPlayer} setTitle ={setTitle}/>}/>
-      <Route path = "/player" element={<Player player = {player} title = {title}/>}/>
-      <Route path = "*"  element={<ErrorPage/>}/>
+        <Route
+          path="/"
+          exact
+          element={
+            <Page1 movie={movie} setPlayer={setPlayer} setTitle={setTitle} />
+          }
+        />
+        <Route
+          path="/player"
+          element={<Player player={player} title={title} />}
+        />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
       <Footer />
     </div>
